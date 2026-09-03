685e362185e36c65a555a851d544ecd2db613870
faisalnugroho <vantjxrg@gmail.com>
2026-09-03 16:40:12 +0800

    ci: vendor genvm std-lib + runner artifacts (11rhn hash, allow_storage intact)
    Upstream genvm release tag v0.3.0-rc7 was re-published with different
    std-lib contents (10pqy... dropped allow_storage from the top-level
    genlayer export). Pin-by-tag cannot protect against re-published
    assets, so vendor the exact extracted artifacts the contract is
    validated against and seed the gltest cache from them.

---
+++ vendor/genvm-artifacts/extracted/v0.3.0-rc7/py-lib-genlayer-std/11rhn002yfajawsz7fai6mykznbxkxs6l91iskj5cm82c92qhy3v/genlayer/__init__.py
@@ -0,0 +1,113 @@
+"""
+Common import for all contracts
+
+It exposes most of the types to the top scope and encapsulates other utility under :py:obj:`gl` namespace which is a proxy to :py:mod:`genlayer.gl`
+"""
+
+__all__ = (
+	'gl',
+	'Address',
+	'allow_storage',
+	'Array',
+	'DynArray',
+	'Keccak256',
+	'TreeMap',
+	'bigint',
+	'i104',
+	'i112',
+	'i120',
+	'i128',
+	'i136',
+	'i144',
+	'i152',
+	'i16',
+	'i160',
+	'i168',
+	'i176',
+	'i184',
+	'i192',
+	'i200',
+	'i208',
+	'i216',
+	'i224',
+	'i232',
+	'i24',
+	'i240',
+	'i248',
+	'i256',
+	'i32',
+	'i40',
+	'i48',
+	'i56',
+	'i64',
+	'i72',
+	'i8',
+	'i80',
+	'i88',
+	'i96',
+	'u104',
+	'u112',
+	'u120',
+	'u128',
+	'u136',
+	'u144',
+	'u152',
+	'u16',
+	'u160',
+	'u168',
+	'u176',
+	'u184',
+	'u192',
+	'u200',
+	'u208',
+	'u216',
+	'u224',
+	'u232',
+	'u24',
+	'u240',
+	'u248',
+	'u256',
+	'u32',
+	'u40',
+	'u48',
+	'u56',
+	'u64',
+	'u72',
+	'u8',
+	'u80',
+	'u88',
+	'u96',
+)
+
+import os
+
+from .py.types import *
+from .py.storage import *
+
+_gen_docs = os.getenv('GENERATING_DOCS', 'false') == 'true'
+
+if not typing.TYPE_CHECKING and not _gen_docs:
+
+	class GL:
+		"""
+		proxy to :py:mod:`genlayer.gl` used for lazy loading
+		"""
+
+		# unfortunately, we have to cache it because with star import it won't be auto-substituted on the first access
+		_cached_gl = None
+
+		def __getattr__(self, attr):
+			if self._cached_gl is None:
+				globals().pop('gl', None)
+				import genlayer.gl as _imp
+
+				# below is needed to trick cloudpickle
+				globals()['gl'] = _imp
+				self._cached_gl = _imp
+
+			return getattr(self._cached_gl, attr)
+
+	gl = GL()
+	del GL
+else:
+	import genlayer.gl as gl
